7650de094a51df7ecc372bc84d34dd895de8e641
zajkoss <lukasz100.96@gmail.com>
2026-07-01 22:27:59 +0200

    feat: add ciruit breaker - redis

--- BookingApi/Services/CacheService.cs
+++ BookingApi/Services/CacheService.cs
@@ -1,22 +1,44 @@
 using System.Text.Json;
 using Microsoft.Extensions.Caching.Distributed;
+using Polly;
+using Polly.CircuitBreaker;
 
 namespace BookingApi.Services;
 
 public class CacheService : ICacheService
 {
     private readonly IDistributedCache _cache;
+    private readonly ResiliencePipeline _pipeline;
 
     public CacheService(IDistributedCache cache)
     {
         _cache = cache;
+        _pipeline = new ResiliencePipelineBuilder()
+            .AddCircuitBreaker(new CircuitBreakerStrategyOptions
+            {
+                FailureRatio = 0.5,
+                SamplingDuration = TimeSpan.FromSeconds(10),
+                MinimumThroughput = 5,
+                BreakDuration = TimeSpan.FromSeconds(30)
+            })
+            .Build();
     }
 
     public async Task<T?> GetAsync<T>(string key)
     {
-        var data = await _cache.GetAsync(key.ToLower());
-        if (data is null) return default;
-        return JsonSerializer.Deserialize<T>(data);
+        try
+        {
+            return await _pipeline.ExecuteAsync(async ct =>
+            {
+                var data = await _cache.GetAsync(key.ToLower());
+                if (data is null) return default;
+                return JsonSerializer.Deserialize<T>(data);
+            });
+        }
+        catch (BrokenCircuitException e)
+        {
+            return default;
+        }
     }
 
     public async Task SetAsync<T>(string key, T value, TimeSpan? expiry = null)
@@ -25,11 +47,30 @@ public class CacheService : ICacheService
         {
             AbsoluteExpirationRelativeToNow = expiry ?? TimeSpan.FromMinutes(10)
         };
-        await _cache.SetAsync(key.ToLower(), JsonSerializer.SerializeToUtf8Bytes(value), options);
+
+        try
+        {
+            _pipeline.ExecuteAsync(async ct =>
+            {
+                var data = await _cache.GetAsync(key.ToLower());
+                await _cache.SetAsync(key.ToLower(), JsonSerializer.SerializeToUtf8Bytes(value), options);
+            });
+        }
+        catch (BrokenCircuitException e)
+        {
+            //Nothing to do
+        }
     }
 
     public async Task RemoveAsync(string key)
     {
-        await _cache.RemoveAsync(key.ToLower());
+        try
+        {
+            await _pipeline.ExecuteAsync(async ct => { await _cache.RemoveAsync(key.ToLower()); });
+        }
+        catch (BrokenCircuitException e)
+        {
+            //Nothing to do
+        }
     }
 }
